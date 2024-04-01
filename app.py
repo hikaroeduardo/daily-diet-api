7f232e318b2b9d4e2095775fc968a06d4fe633f4
Hikaro <hikaroeduardobandeira@gmail.com>
2024-04-01 15:30:21 -0300

    feat: create new user with encrypted password

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from database import db
 from models.models import User, Food
+import bcrypt
 
 app = Flask(__name__)
 
@@ -8,9 +9,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return jsonify({ "Message": "Hello World" })
+@app.route('/register', methods=['POST'])
+def create_user():
+    try:
+        data = request.json
+        
+        name = data["name"]
+        email= data["email"]
+        password = data["password"]
+
+        user = User(name=name, email=email, password=bcrypt.hashpw(str.encode(password), bcrypt.gensalt()))
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({ "Message": "Usuário cadastrado com sucesso!" })
+    except KeyError:
+        return jsonify({ "Message": "Todos os campos são obrigatórios." }), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
